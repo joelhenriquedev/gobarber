3386caaf8eddac04d0948cc6b46869f5522f2bf3
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-01-25 12:22:58 -0300

    Cadastro de usuários

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Joel Henrique',
-    email: 'joelhenrique2000@gmail.com',
-    password_hash: '209rjsdhas298',
-    provider: false,
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
