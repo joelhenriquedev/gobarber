33b92234a62029bd184b921804330e6595e0f123
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-01-25 10:50:43 -0300

    Criando o loader de models

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Joel Henrique',
+    email: 'joelhenrique2000@gmail.com',
+    password_hash: '209rjsdhas298',
+    provider: false,
+  });
+
+  return res.json(user);
 });
 
 export default routes;
