e1e533b2506d3a4a9198013b5facb2900e6697eb
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-02-01 21:32:47 -0300

    Configurando Nodemailer

---
+++ api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525 ',
+  secure: false,
+  auth: {
+    user: '37947de6841ba3',
+    pass: '4da3dca80a2f2a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
